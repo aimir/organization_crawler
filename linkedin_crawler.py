8aa6cc15f550709fc499dcabd3f5ac29539c0ec6
Edo Arad <edoarad92@gmail.com>
2023-01-20 15:33:13 +0200

    linkedin on all in list, need to clear things up

--- linkedin_crawler.py
+++ linkedin_crawler.py
@@ -1,20 +1,29 @@
 from linkedin_api import Linkedin
 import getpass
 from functools import reduce
-import sys
 
-username = 'amirsarid1@gmail.com'
-password = getpass.getpass()
-AAA = [b'firstime-vc', b'pitango-vc', b'trendlines', b'amoon-fund', b'impact-first-investments', b'bridges-israel', b'elahfund', b'zora', b'cactus-capital', b'israel-venture-network', b'arc-impact', b'good-company-mission-driven', b'gandyr-investments-group', b'champel-capital', b'menomadin-foundation', b'neweracp', b'terra-venture-partners', b'takwin-vc', b'morevc', b'vital-capital', b'micaventures', b'12635226', b'sibf-vc', b'kinneret-impact-ventures', b'bits-x-bites', b'shift-invest', b'2bvc', b'five-seasons-ventures', b'ngt-technology', b'jvp', b'28600695', b'hanacoventures', b'ourcrowd-llc', b'the-founders-kitchen', b'8vc']
-
-company = sys.argv[-1]
+username = getpass.getpass(prompt='Email: ')
+password = getpass.getpass(prompt='Password: ')
 linkedin = Linkedin(username, password)
-records = linkedin.get_company_updates(company)
-get_attrs = lambda x: (x['value']['com.linkedin.voyager.feed.render.UpdateV2']['commentary']['text'].get('attributes', [])
-                       if 'resharedUpdate' not in x['value']['com.linkedin.voyager.feed.render.UpdateV2'] else [])
-linked_company = reduce(lambda x, y: x + y,
-                        [
-                            [attr['miniCompany']['universalName'] for attr in get_attrs(r) if attr['type'] == 'COMPANY_NAME']
-                        for r in records],
-                        [])
-print(company, len(set(linked_company)))
+AAA = [b'firstime-vc', b'pitango-vc', b'trendlines', b'amoon-fund', b'impact-first-investments', b'bridges-israel', b'elahfund', b'zora', b'cactus-capital', b'israel-venture-network', b'arc-impact', b'good-company-mission-driven', b'gandyr-investments-group', b'champel-capital', b'menomadin-foundation', b'neweracp', b'terra-venture-partners', b'takwin-vc', b'morevc', b'vital-capital', b'micaventures', b'12635226', b'sibf-vc', b'kinneret-impact-ventures', b'bits-x-bites', b'shift-invest', b'2bvc', b'five-seasons-ventures', b'ngt-technology', b'jvp', b'28600695', b'hanacoventures', b'ourcrowd-llc', b'the-founders-kitchen', b'8vc']
+aaa = AAA[:3]
+result = {}
+for company in aaa:
+
+    records = linkedin.get_company_updates(company)
+    get_attrs = lambda x: (x['value']['com.linkedin.voyager.feed.render.UpdateV2']['commentary']['text'].get('attributes', [])
+                        if 'resharedUpdate' not in x['value']['com.linkedin.voyager.feed.render.UpdateV2'] else [])
+    linked_company = reduce(lambda x, y: x + y,
+                            [
+                                [attr['miniCompany']['universalName'] for attr in get_attrs(r) 
+                                                                        if attr['type'] == 'COMPANY_NAME']
+                            for r in records],
+                            [])
+    result[company] = linked_company
+
+# TODO: make this fucking readable
+rrr = {(a, b): len([c for c in result[a] if c.encode('ASCII', errors="replace") == b and b != a]) for a in aaa for b in AAA}
+for key in list(rrr.keys())[:]:
+    if rrr[key] == 0:
+        rrr.pop(key)
+print(rrr)

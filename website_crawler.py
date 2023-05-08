56fada7902b405f9ed3cfecd9d608a09cef84ecb
Edo Arad <edoarad92@gmail.com>
2023-05-08 12:26:33 +0300

    minor quality-of-life improvements, and defraging

--- website_crawler.py
+++ website_crawler.py
@@ -14,8 +14,8 @@ REAL_UA = ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 URL_REGEX1 = b'href *?= *?[\'"](.*?)([\'"])'
 URL_REGEX2 = (b'(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}'
               b'(/[-a-zA-Z0-9()@:%_\+.~#?&//=]*)?)')
-FORBIDDEN_URL_SUBSTRINGS = [b'.jpg', b'.jpeg', b'.png', b'.gif', b'.bmp', b'.woff', b'.woff2',
-                            b'.css', b'.ttf', b'.mp3', b'.mp4', b'.wav', b'.map', b'.ico', b'.pdf']
+FORBIDDEN_URL_SUBSTRINGS = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.woff', '.woff2',
+                            '.css', '.ttf', '.mp3', '.mp4', '.wav', '.map', '.ico', '.pdf']
 ALLOWED_CONTENT = ['text/plain', 'application/json', 'text/html', 'application/javascript',
                    'text/javascript', 'text/csv', 'application/xhtml+xml', 'application/xml',
                    'text/xml', 'application/rss+xml', 'application/x-javascript']
@@ -31,14 +31,14 @@ FORBIDDEN_CONTENT = ['application/octet-stream', 'font/woff2', 'font/woff', 'ima
                      'application/rdf+xml']
 # How long to wait before the first request, between the first and second, etc.
 BACKOFF_REQUEST_TIMES = [0, 0, 0]
-ALLOWED_SCHEMES = [b'http', b'https']
-FORBIDDEN_DOMAINS = [b'unpkg.com', b'fonts.googleapis.com', b'www.youtube.com', b'www.w3.org',
-                     b'www.linkedin.com', b'www.facebook.com', b'www.google.com',
-                     b'polymer.github.io', b'open.spotify.com', b'underscorejs.org',
-                     b'openjsf.org', b'npms.io', b'fb.me', b'mailchi.mp', b'api.whatsapp.com',
-                     b'sentry.io', b'secure.gravatar.com', b'github.com', b'lodash.com',
-                     b'discord.gg', b'kit.fontawesome.com', b'il.linkedin.com', b'api.jqueryui.com',
-                     b'getbootstrap.com', 'getbootstrap.com']
+ALLOWED_SCHEMES = ['http', 'https']
+FORBIDDEN_DOMAINS = ['unpkg.com', 'fonts.googleapis.com', 'www.youtube.com', 'www.w3.org',
+                     'www.linkedin.com', 'www.facebook.com', 'www.google.com',
+                     'polymer.github.io', 'open.spotify.com', 'underscorejs.org',
+                     'openjsf.org', 'npms.io', 'fb.me', 'mailchi.mp', 'api.whatsapp.com',
+                     'sentry.io', 'secure.gravatar.com', 'github.com', 'lodash.com',
+                     'discord.gg', 'kit.fontawesome.com', 'il.linkedin.com', 'api.jqueryui.com',
+                     'getbootstrap.com', 'getbootstrap.com', 'microsoft.com']
 MAX_DEPTH = 3
 CONNECTION_TIMEOUT = 10 # seconds
 
@@ -69,42 +69,50 @@ def extract_links(base_url):
         return []
 
     # Filter relevant urls:
-    likely_urls = set([match[0] for match in (re.findall(URL_REGEX1, resp.content) +
+    likely_urls = set([match[0].decode('utf-8') for match in (re.findall(URL_REGEX1, resp.content) +
                                               re.findall(URL_REGEX2, resp.content))
-                                              if all(word not in match[0]
+                                              if all(word not in match[0].decode('utf-8')
                                                      for word in FORBIDDEN_URL_SUBSTRINGS)])
     found_urls = set()
     for likely_url in likely_urls:
         try:
             absolute_url = urllib.parse.urljoin(base_url, likely_url)
-            parts = urllib.parse.urlparse(absolute_url)
+            defraged_url = urllib.parse.urldefrag(absolute_url).url
+            parts = urllib.parse.urlparse(defraged_url)
         except ValueError:
+            print
             pass
         else:
             if (parts.scheme.lower() in ALLOWED_SCHEMES
                 and parts.netloc.lower() not in FORBIDDEN_DOMAINS):
-                found_urls.add(absolute_url)
+                found_urls.add(defraged_url)
     return found_urls
 
-result = {}
+def get_records_by_website(website_names):
+    result = {}
 
-for domain in [b'firstime.vc']:
-    base_url = b'https://' + domain + '/'
-    seen_urls = {base_url}
-    to_query_queue = [(base_url, 0)]
-    scraped_count = 0
+    for domain in website_names:
+        print("Scraping ", domain)
+        base_url = domain
+        if not base_url.startswith('http'):
+            base_url = 'https://' + base_url
+        seen_urls = {base_url}
+        to_query_queue = [(base_url, 0)]
+        scraped_count = 0
 
-    while to_query_queue:
-        # TODO: async requests
-        url, depth = to_query_queue[0]
-        to_query_queue = to_query_queue[1:]
-        for new_url in extract_links(url):
-            # TODO: properly canonicalize URLs
-            if not new_url in seen_urls and depth < MAX_DEPTH:
-                seen_urls.add(new_url)
-                if domain in new_url:
-                    to_query_queue.append((new_url, depth + 1))
-        scraped_count += 1
-        if scraped_count % 10 == 0:
-            print(scraped_count, '/', len(seen_urls))
-    result[domain] = list(seen_urls)
+        while to_query_queue:
+            # TODO: async requests
+            url, depth = to_query_queue[0]
+            to_query_queue = to_query_queue[1:]
+            for new_url in extract_links(url):
+                # TODO: properly canonicalize URLs
+                if not new_url in seen_urls and depth < MAX_DEPTH:
+                    seen_urls.add(new_url)
+                    if domain in new_url:
+                        to_query_queue.append((new_url, depth + 1))
+            scraped_count += 1
+            if scraped_count % 10 == 0:
+                print(scraped_count, '/', len(to_query_queue)+scraped_count, " [found " + str(len(seen_urls)) + "]")
+        result[domain] = list(seen_urls)
+    return result
+
